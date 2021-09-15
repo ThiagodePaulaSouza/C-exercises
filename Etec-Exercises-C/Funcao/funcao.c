10eae6e78f8fcc5f233a16ac7c0f872fc5ce2623
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2021-09-15 18:45:10 -0300

    ex3

--- Etec-Exercises-C/Funcao/funcao.c
+++ Etec-Exercises-C/Funcao/funcao.c
@@ -3,23 +3,46 @@
 
 void mensagem(void)
 {
-    printf("**********ex1*************");
-    printf("\nMedidor de Raio Pannunzio\n");
+    printf("***********************");
+    printf("\nETEC Armando Pannunzio\n");
     printf("***********************\n");
 }
 
-int calcular(int raio)
+int soma(int n1, int n2)
 {
-    int area;
-    area = 3.14*(raio * raio);
-    return (area);
+    int adicao;
+    adicao = n1 + n2;
+    return (adicao);
 }
-int main()
+
+int subtracao(int n1, int n2)
 {
-    int raio, area;
+    int diferenca;
+    diferenca = n1 - n2;
+    return (diferenca);
+}
+
+int multiplicacao(int n1, int n2)
+{
+    int produto;
+    produto = n1 * n2;
+    return (produto);
+}
+
+void main()
+{
+    int v1, v2, adicao, diferenca, produto;
+    //chamar a funcao mensagem
     mensagem();
-    printf("\nDigite o valor do raio: ");
-    scanf("%i", &raio);
-    area = calcular(raio);
-    printf("\nArea = %i\n", area);
+    printf("\nDigite o primeiro valor: ");
+    scanf("%i", &v1);
+    printf("\nDigite o primeiro valor: ");
+    scanf("%i", &v2);
+    //chamar a funcao multiplicacao
+    adicao = soma(v1, v2);
+    diferenca = subtracao(v1, v2);
+    produto = multiplicacao(v1, v2);
+    printf("\nResultado da soma = %d\n", adicao);
+    printf("\nResultado da subtracao = %d\n", diferenca);
+    printf("\nResultado da multiplicacao = %d\n", produto);
 }
